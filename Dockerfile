5ea6825ce51378e975244de4f34ed7a7a135bc9a
AnnegretSe <anniluenchen@googlemail.com>
2022-05-01 14:35:12 +0200

    HTTP Go Docker Hello World

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,17 @@
 FROM golang:1.18
 
-WORKDIR /usr/src/app
+WORKDIR /app
 
 # pre-copy/cache go.mod for pre-downloading dependencies and only redownloading them in subsequent builds if they change
 COPY src/go.mod ./
-RUN go mod download && go mod verify
+RUN go mod download
 
 COPY src/main.go ./
-RUN go build -v -o /usr/local/bin/app ./...
 
-CMD ["app"]
+RUN go build -o /hello_world_http
+
+# Make sure to expose the port the HTTP server is using
+EXPOSE 8080
+
+# Run the app binary when we run the container
+ENTRYPOINT ["/hello_world_http"]
--- src/main.go
+++ src/main.go
@@ -2,17 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 func main() {
 	var a, c, b, sum int
+	var response string
 
 	a = 4
 	b = 2
 	c = 1
 	sum = a * b * c
 
-	fmt.Println("Little go test")
-	fmt.Println(sum)
+	response = "hello World " + strconv.Itoa(sum)
+
+	//fmt.Println("Little go test")
+	//fmt.Println(sum)
+
+	http.HandleFunc("/helloworld", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintln(writer, response)
+	})
+	fmt.Printf("Server running (port=8080), route: http://localhost:8080/helloworld\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 
 }
