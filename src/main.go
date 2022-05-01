3388668a66c9e237fc02eb840739523c66f88fc4
AnnegretSe <anniluenchen@googlemail.com>
2022-05-01 14:53:18 +0200

    Hello World with Html Template

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strconv"
@@ -18,15 +19,27 @@ func main() {
 
 	response = "hello World " + strconv.Itoa(sum)
 
-	//fmt.Println("Little go test")
+	//fmt.Println("Little go test") print a line on console
 	//fmt.Println(sum)
 
 	http.HandleFunc("/helloworld", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintln(writer, response)
+		//fmt.Fprintln(writer, response) print a line fot http
+
+		data := struct {
+			TEXT string
+		}{response}
+		tmpl.Execute(writer, data) //execute a html template
 	})
+
 	fmt.Printf("Server running (port=8080), route: http://localhost:8080/helloworld\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 
 }
+
+var tmpl = template.Must(template.New("tmpl").Parse(`
+<!DOCTYPE html><html><body><center>
+	<h2>{{.TEXT}}</h2>
+</center></body></html>
+`))
